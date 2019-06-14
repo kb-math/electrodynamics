bc7347f39caeb2ac9a9272724d98f0f42a5427e3
kb-math <kamil.bulinski@gmail.com>
2019-06-14 23:25:33 +0930

    Sinusoidal models

--- simulation.py
+++ simulation.py
@@ -1,14 +1,15 @@
 from lienard_wiechert import *
+import kinematics.motion_lib as motion_lib
 
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-dt_loop = 1.50
+dt = 1.0
 
 # how many metres represents one newton per coulomb
-ELECTRIC_FIELD_SCALING = 0.6
+ELECTRIC_FIELD_SCALING = 0.1
 
 
 class FieldTail(object):
@@ -40,6 +41,8 @@ if __name__ == '__main__':
 	fig = plt.figure()
 	fig.show()
 
+	charge_motion = motion_lib.Sinusoidal(freq = 0.1, max_speed = 0.9 * SPEED_OF_LIGHT)
+
 	charge_position = np.array([0.0, 0.0, 0.0])
 	charge_velocity = np.array([0.0, 0.0, 0.0])
 	charge_acceleration = np.array([1.0, 0.0, 0.0])
@@ -49,34 +52,42 @@ if __name__ == '__main__':
 
 	radius = 1.0
 
-	current_time = dt_loop
-	
+	current_time = 0.0
+	br = 0
 
 	#TODO: factor in general speed of light and consequent retardation
 	while (True):
 		plt.clf()
-		for unit_point in points_unit_circle:
-			field_tails.append(FieldTail(unit_point, charge_position, charge_velocity, charge_acceleration))
+		charge_kinematics = charge_motion.get_state(current_time)
+		charge_position = np.array(charge_kinematics.position)
+		charge_velocity = np.array(charge_kinematics.velocity)
+		charge_acceleration = np.array(charge_kinematics.acceleration)
+
+		should_plot_electric_field = (br % 2 == 0)
+		if should_plot_electric_field:
+			for unit_point in points_unit_circle:
+				field_tails.append(FieldTail(unit_point + charge_position, charge_position, charge_velocity, charge_acceleration))
 
 		for field_tail in field_tails:
 			E_vector = field_tail.E_vector()
 			E_vector *= ELECTRIC_FIELD_SCALING
 			E_vector_length = np.linalg.norm(E_vector)
 			x = np.array(field_tail._position)
-			plt.arrow(x[0], x[1], E_vector[0], E_vector[1], head_width = 0.5, head_length = 0.5)
-
-			plt.scatter(charge_position[0], charge_position[1], color = 'blue')
+			#plt.arrow(x[0], x[1], E_vector[0], E_vector[1], head_width = 0.5, head_length = 0.5)
+			plt.quiver(x[0], x[1], E_vector[0], E_vector[1])
 
 			#TODO: be careful of sneaky python pass by reference?
-
-			field_tail.advance(dt_loop)
+			field_tail.advance(dt)
 
 			#TODO: if we run this forever, we should probably clear points too far apart (should be ordered)
 			#solution: flag the index of a field_tail really close to boundary and delete all after that index
 
+		plt.scatter(charge_position[0], charge_position[1], color = 'blue')
+		
 		plt.axis([-10, 10, -10, 10])
 		fig.canvas.draw()
 
-		#radius += dt_loop * SPEED_OF_LIGHT
-		time.sleep(dt_loop)
+		br += 1
+		current_time += dt
+		time.sleep(dt)
 
